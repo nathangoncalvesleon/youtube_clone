0175f657c80eb929e030aeea180135952a76c1e3
nathangoncalvesleon <nathangworks@gmail.com>
2022-08-27 11:59:31 -0300

    inicio

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Box } from "@mui/material";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Feed from "./components/Feed";
+import VideoDetail from './components/VideoDetail';
+import ChannelDetail from './components/ChannelDetail';
+import SearchFeed from './components/SearchFeed';
+import Navbar from './components/Navbar';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id"  element={<VideoDetail />} />
+          <Route path="/channel/:id"  element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm"  element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { logo } from '../utils/constants';
+
+export default function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
